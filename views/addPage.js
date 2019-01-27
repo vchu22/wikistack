118b59747ce56bdb59883e2186046f119dc94768
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2019-01-27 10:25:59 -0500

    edited addPage

--- views/addPage.js
+++ views/addPage.js
@@ -6,10 +6,17 @@ module.exports = () =>
     <h3>Add a Page</h3>
     <hr />
     <form method="POST" action="/wiki/">
-      <div>PLACEHOLDER FOR AUTHOR NAME FIELD</div>
+      <div class="form-group">
+        <label for="name" class="col-sm-2 control-label">Author Name</label>
+        <div class="col-sm-10">
+          <input id="name" name="name" type="text" class="form-control" />
+        </div>
 
-      <div>PLACEHOLDER FOR AUTHOR EMAIL FIELD</div>
-
+        <label for="email" class="col-sm-2 control-label">Author Email</label>
+        <div class="col-sm-10">
+          <input id="email" name="email" type="text" class="form-control" />
+        </div>
+      </div>
       <div class="form-group">
         <label for="title" class="col-sm-2 control-label">Page Title</label>
         <div class="col-sm-10">
@@ -18,18 +25,14 @@ module.exports = () =>
 
         <label for="content" class="col-sm-2 control-label">Page Content</label>
         <div class="col-sm-10">
-          <input
-            id="content"
-            name="content"
-            type="textarea"
-            class="form-control"
-          />
+          <textarea id="content" name="content" class="form-control"></textarea>
+        </div>
+
+        <label for="status" class="col-sm-2 control-label">Page Status</label>
+        <div class="col-sm-10">
+          <input id="status" name="status" type="text" class="form-control" />
         </div>
       </div>
-
-      <div>PLACEHOLDER FOR PAGE CONTENT TEXTAREA FIELD</div>
-
-      <div>PLACEHOLDER FOR PAGE STATUS INPUT FIELD</div>
 
       <div class="col-sm-offset-2 col-sm-10">
         <button type="submit" class="btn btn-primary">submit</button>
