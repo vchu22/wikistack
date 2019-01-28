cb3be041ef7f07e007013267b9eb004b7baf8e75
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2019-01-27 23:47:00 -0500

    finished Authors pages

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { Page } = require('../models');
+const { Page, User } = require('../models');
 const { addPage, wikiPage, main } = require('../views');
 
 router.get('/', async (req, res, next) => {
@@ -8,10 +8,19 @@ router.get('/', async (req, res, next) => {
   res.send(main(pages));
 });
 router.post('/', async (req, res, next) => {
-  const page = new Page(req.body);
-  // make sure we only redirect *after* our save is complete!
-  // note: `.save` returns a promise.
   try {
+    const [user, wasCreated] = await User.findOrCreate({
+      where: {
+        name: req.body.name,
+        email: req.body.email,
+      },
+    });
+
+    const page = await Page.create(req.body);
+
+    page.setAuthor(user);
+    // make sure we only redirect *after* our save is complete!
+    // note: `.save` returns a promise.
     await page.save();
     res.redirect(`/wiki/${page.slug}`);
   } catch (error) {
@@ -30,7 +39,8 @@ router.get('/:slug', async (req, res, next) => {
         slug: req.params.slug,
       },
     });
-    res.send(wikiPage(page, 'author'));
+    const author = await page.getAuthor();
+    res.send(wikiPage(page, author));
   } catch (error) {
     next(error);
   }
